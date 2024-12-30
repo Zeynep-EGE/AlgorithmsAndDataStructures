2425ba8ee93e1c927aabb1829e2ab308d5df52c9
Zeynep-EGE <zeynepegekaracabey@gmail.com>
2024-12-30 20:07:19 +0300

    polindrom number

--- Problems/problem 9/script.js
+++ Problems/problem 9/script.js
@@ -1,7 +1,20 @@
 /*Polindrom Sayı : rakamları ters çevrildiğinde aynı kalan bir sayıdır (151)*/
 
-var number = prompt("please enter a number :");
-var transform = toString(number);
-const result = number.split("");
+let num;
+num = prompt('Please enter a number : ');
+let number = num;
+let newNum;
+let numbers = [];
+
+numbers = String(number).split("");
+numbers = numbers.reverse();
+newNum = numbers.join('');
+
+if( newNum == num){
+    console.log(num + ' sayısı bir polindrom number dır');
+} else {
+    console.log(num + ' sayısı bir polindrom number değildir');
+}
+
 
 
